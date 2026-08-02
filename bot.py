8ca77b69abbf3a2fb606056971579db75208f7ff
243550020-source <243550020@ogr.dicle.edu.tr>
2026-08-02 19:14:10 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -125,7 +125,7 @@ def download_audio(url):
         return mp3_filename
 
 if __name__ == '__main__':
-    TOKEN = "8222625062:AAH-GZ2GCLNcQE0YS_fAQekyBgDuiGxv0p8"  # Kendi bot token'ını buraya yazdığından emin ol
+    TOKEN = "8222625062:AAHBtlhXR-5VhUGDksLm4j475Y4ulB2ftzY"  # Kendi bot token'ını buraya yazdığından emin ol
     
     app = ApplicationBuilder().token(TOKEN).build()
     
